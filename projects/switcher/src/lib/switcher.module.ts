3d539fe531027df5748862d97c73a92cc2edb2d6
jitendershiprocket <jitender.sharma@kartrocket.com>
2024-11-25 16:58:55 +0530

    add angular element for react

--- projects/switcher/src/lib/switcher.module.ts
+++ projects/switcher/src/lib/switcher.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SwitcherComponent } from './switcher.component';
+import { createCustomElement } from '@angular/elements';
+
 
 @NgModule({
   declarations: [SwitcherComponent],
   imports: [CommonModule],
   exports: [SwitcherComponent] // Export the component for use in other modules
 })
-export class SwitcherModule {}
+export class SwitcherModule {
+  constructor(private injector: Injector) {
+    const customElement = createCustomElement(SwitcherComponent, { injector });
+    customElements.define('switcher-element', customElement);
+  }
+}
